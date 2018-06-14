c6a18b49d60adde5f830035203855cae3a21fcd7
cyunzing <cyunzing@gmail.com>
2018-06-14 17:48:36 +0800

    multi light

--- lighting/multilight/multilight.cpp
+++ lighting/multilight/multilight.cpp
@@ -4,6 +4,8 @@
 
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+#include <iostream>
+#include <sstream>
 
 #include "../../common/shader.h"
 #include "../../common/texture.h"
@@ -15,8 +17,6 @@ Camera camera(glm::vec3(0.0f, 0.0f, 4.0f));
 GLboolean keyStatus[1024] = { GL_FALSE };
 GLfloat deltaTime = 0.0f, lastFrame = 0.0f;
 GLboolean buttonPressed = GL_FALSE;
-
-glm::vec3 lampPosition(0.8f, 0.8f, 0.5f);
 
 void keyboard(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow *window = glfwCreateWindow(WINDOW_SIZE, WINDOW_SIZE, "Diffuse map", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WINDOW_SIZE, WINDOW_SIZE, "Spot light soft edge", NULL, NULL);
     if (!window)
         return -1;
 
@@ -158,6 +158,26 @@ int main(int argc, char *argv[])
         -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,  0.0f, -1.0f, 0.0f, // A
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)
+    };
+
+    glm::vec3 pointLightPositions[] = {
+        glm::vec3(0.7f, 0.2f, 2.0f),
+        glm::vec3(2.3f, -3.3f, -4.0f),
+        glm::vec3(-4.0f, 2.0f, -12.0f),
+        glm::vec3(0.0f, 0.0f, -3.0f)
+    };
+
     GLuint vaoId, vboId;
     glGenVertexArrays(1, &vaoId);
     glBindVertexArray(vaoId);
@@ -172,16 +192,6 @@ int main(int argc, char *argv[])
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid *)(5 * sizeof(GL_FLOAT)));
     glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    GLuint lampVaoId;
-    glGenVertexArrays(1, &lampVaoId);
-    glBindVertexArray(lampVaoId);
-    glBindBuffer(GL_ARRAY_BUFFER, vboId);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid *)0);
-    glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -214,14 +224,42 @@ int main(int argc, char *argv[])
         glBindVertexArray(vaoId);
         shader.use();
 
-        // 设置光源属性
-		GLfloat radius = 2.0f;
-		lampPosition.x = radius * cos(glfwGetTime());
-		lampPosition.z = radius * sin(glfwGetTime());
-        glUniform3f(glGetUniformLocation(shader.programId, "light.ambient"), 0.2f, 0.2f, 0.2f);
-        glUniform3f(glGetUniformLocation(shader.programId, "light.diffuse"), 0.5f, 0.5f, 0.5f);
-        glUniform3f(glGetUniformLocation(shader.programId, "light.specular"), 1.0f, 1.0f, 1.0f);
-        glUniform3f(glGetUniformLocation(shader.programId, "light.position"), lampPosition.x, lampPosition.y, lampPosition.z);
+        // 设置方向光源
+        glm::vec3 lampDirection(0.5f, 0.8f, 0.0f);
+        glUniform3f(glGetUniformLocation(shader.programId, "dlight.ambient"), 0.0f, 0.1f, 0.4f);
+        glUniform3f(glGetUniformLocation(shader.programId, "dlight.diffuse"), 0.0f, 0.2f, 0.8f);
+        glUniform3f(glGetUniformLocation(shader.programId, "dlight.specular"), 0.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(shader.programId, "dlight.direction"), lampDirection.x, lampDirection.y, lampDirection.z); // 方向光源
+
+        // 设置点光源
+        for (int i = 0; i < sizeof(pointLightPositions) / sizeof(pointLightPositions[0]); ++i) {
+            // 设置光源属性 点光源
+            std::stringstream idx;
+            idx << i;
+            std::string lightName = "plights[" + idx.str() + "]";  // 使用数组索引plights[i]
+            glUniform3f(glGetUniformLocation(shader.programId, (lightName + ".ambient").c_str()), 0.0f, 0.1f, 0.4f);
+            glUniform3f(glGetUniformLocation(shader.programId, (lightName + ".diffuse").c_str()), 0.0f, 0.1f, 0.6f);
+            glUniform3f(glGetUniformLocation(shader.programId, (lightName + ".specular").c_str()), 0.0f, 1.0f, 1.0f);
+            glm::vec3 lampPos = pointLightPositions[i];
+            glUniform3f(glGetUniformLocation(shader.programId, (lightName + ".position").c_str()), lampPos.x, lampPos.y, lampPos.z);
+            // 设置衰减系数
+            glUniform1f(glGetUniformLocation(shader.programId, (lightName + ".constant").c_str()), 1.0f);
+            glUniform1f(glGetUniformLocation(shader.programId, (lightName + ".linear").c_str()), 0.09f);
+            glUniform1f(glGetUniformLocation(shader.programId, (lightName + ".quadratic").c_str()), 0.032f);
+        }
+
+        // 设置光源属性 FlashLight光源
+        glUniform3f(glGetUniformLocation(shader.programId, "slight.ambient"), 0.0f, 0.1f, 0.4f);
+        glUniform3f(glGetUniformLocation(shader.programId, "slight.diffuse"), 0.0f, 0.2f, 0.8f);
+        glUniform3f(glGetUniformLocation(shader.programId, "slight.specular"), 0.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(shader.programId, "slight.position"), camera.position.x, camera.position.y, camera.position.z);
+        glUniform3f(glGetUniformLocation(shader.programId, "slight.direction"), camera.forward.x, camera.forward.y, camera.forward.z);
+        glUniform1f(glGetUniformLocation(shader.programId, "slight.cutoff"), cos(glm::radians(12.5f)));
+        glUniform1f(glGetUniformLocation(shader.programId, "slight.outerCutoff"), cos(glm::radians(17.5f)));
+        // 设置衰减系数
+        glUniform1f(glGetUniformLocation(shader.programId, "slight.constant"), 1.0f);
+        glUniform1f(glGetUniformLocation(shader.programId, "slight.linear"), 0.09f);
+        glUniform1f(glGetUniformLocation(shader.programId, "slight.quadratic"), 0.032f);
 
         // 设置材料光照属性
         glActiveTexture(GL_TEXTURE0);
@@ -236,22 +274,18 @@ int main(int argc, char *argv[])
         glUniform3f(glGetUniformLocation(shader.programId, "viewPosition"), camera.position.x, camera.position.y, camera.position.z);
 
         // 设置变换矩阵
-        glm::mat4 model;
-        //model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         glUniformMatrix4fv(glGetUniformLocation(shader.programId, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(shader.programId, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        glBindVertexArray(lampVaoId);
-        lampShader.use();
-        model = glm::mat4();
-        model = glm::translate(model, lampPosition);
-        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
-        glUniformMatrix4fv(glGetUniformLocation(lampShader.programId, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(glGetUniformLocation(lampShader.programId, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(glGetUniformLocation(lampShader.programId, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glm::mat4 model;
+        for (int i = 0; i < sizeof(cubePositions) / sizeof(cubePositions[0]); ++i) {
+            model = glm::mat4();
+            model = glm::translate(model, cubePositions[i]);
+            GLfloat angle = 20.0f * i;
+            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glBindVertexArray(0);
         glUseProgram(0);
@@ -261,7 +295,6 @@ int main(int argc, char *argv[])
 
     glDeleteVertexArrays(1, &vaoId);
     glDeleteBuffers(1, &vboId);
-    glDeleteVertexArrays(1, &lampVaoId);
 
     glfwTerminate();
 
